ad81c3b475cdafaa0eccc633918cbdf0d1b1f115
Aasal Almaimani <almaimaniaasal@gmail.com>
2025-10-26 16:48:40 +0300

    Player Movement with Animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -9,21 +10,44 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float speed;
     private Rigidbody2D rb;
     private Vector2 moveDirection;
+    private Animator anim;
     
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Input
         moveDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if (Mathf.Abs(moveDirection.x) > Mathf.Abs(moveDirection.y))
+        {
+            moveDirection.y = 0;
+        }
+        else
+        {
+            moveDirection.x = 0;       
+        }
+        
+        anim.SetFloat("Horizontal", moveDirection.x);
+        anim.SetFloat("Vertical", moveDirection.y);
+        anim.SetFloat("Speed", moveDirection.sqrMagnitude);
+
+        if (moveDirection != Vector2.zero)
+        {
+            anim.SetFloat("LastHorizonatal", moveDirection.x);
+            anim.SetFloat("LastVertical", moveDirection.y);
+        }
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = moveDirection * speed;
+        //Movement
+        rb.MovePosition(rb.position + moveDirection * speed);
     }
 }
