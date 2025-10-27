7d67935e3ac7bc5b262ee14d1eade879d3369d5e
Aasal Almaimani <almaimaniaasal@gmail.com>
2025-10-27 20:28:04 +0300

    Sound Manager

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
+    }
+}
